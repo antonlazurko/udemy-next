58d6b91486da8d10b7e1985adfcf02bc84fa29f2
Bogdan Stashchuk <bstashchuk@gmail.com>
2024-05-16 22:29:21 +0100

    Remove rare categories after import, add indexes and add readme file for the seed process

--- server/database/models/quoteCategory.js
+++ server/database/models/quoteCategory.js
@@ -1,7 +1,16 @@
 const sequelize = require('.');
 
-const QuoteCategory = sequelize.define('QuoteCategory', {
-  // No need for any additional attributes in this table
-});
+const QuoteCategory = sequelize.define(
+  'QuoteCategory',
+  {
+    // No need for any additional attributes in this table
+  },
+  {
+    indexes: [
+      { name: 'QuoteCategories_CategoryId', fields: ['CategoryId'] },
+      { name: 'QuoteCategories_QuoteId', fields: ['QuoteId'] },
+    ],
+  }
+);
 
 module.exports = QuoteCategory;
--- server/database/seed/sampleSQLQueries.sql
+++ server/database/seed/sampleSQLQueries.sql
@@ -1,3 +1,8 @@
+-- Find total qty of the rows in the Categories table
+SELECT
+    COUNT(*) AS category_count
+FROM
+    "Categories";
 
 -- Find how many quotes have specific category
 SELECT
@@ -6,16 +11,14 @@ SELECT
     COUNT("QuoteCategories"."CategoryId") AS qty_of_quotes
 FROM
     "Categories"
-LEFT JOIN
-    "QuoteCategories" ON "Categories".id = "QuoteCategories"."CategoryId"
+    LEFT JOIN "QuoteCategories" ON "Categories".id = "QuoteCategories"."CategoryId"
 GROUP BY
-    "Categories".id, "Categories".name
+    "Categories".id,
+    "Categories".name
 ORDER BY
     qty_of_quotes DESC
 LIMIT
     100;
-
-
 
 -- Find total qty of the categories which have just one quote
 -- You can change number to other to find info for other categories
@@ -33,7 +36,6 @@ FROM
             COUNT(*) = 1
     ) AS single_quote_categories;
 
-
 -- Find names of the categories which appear only in one quote
 -- You could modify 1 to other value to see categories which appear in multiple quotes (for example 5)
 SELECT
@@ -49,5 +51,23 @@ FROM
         HAVING
             COUNT(*) = 1
     ) AS single_quote_categories
-JOIN
-    "Categories" ON "Categories".id = single_quote_categories."CategoryId";
+    JOIN "Categories" ON "Categories".id = single_quote_categories."CategoryId";
+
+-- Find Ids of the categories which appear in one or two quotes
+SELECT
+    "CategoryId"
+FROM
+    "QuoteCategories"
+GROUP BY
+    "CategoryId"
+HAVING
+    COUNT(*) <= 2;
+
+-- Find quotes without categories (they might be left after removal of the rare categories)
+SELECT
+    "Quotes".*
+FROM
+    "Quotes"
+    LEFT JOIN "QuoteCategories" ON "Quotes".id = "QuoteCategories"."QuoteId"
+WHERE
+    "QuoteCategories"."CategoryId" IS NULL;
