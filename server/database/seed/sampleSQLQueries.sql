1cb131eb2b5f407dc5eca81eec3d35139c9fe4c1
Bogdan Stashchuk <bstashchuk@gmail.com>
2024-05-16 13:13:43 +0100

    Add sample SQL queries for Categories and Quotes

---
+++ server/database/seed/sampleSQLQueries.sql
@@ -0,0 +1,53 @@
+
+-- Find how many quotes have specific category
+SELECT
+    "Categories".id AS category_id,
+    "Categories".name AS category_name,
+    COUNT("QuoteCategories"."CategoryId") AS qty_of_quotes
+FROM
+    "Categories"
+LEFT JOIN
+    "QuoteCategories" ON "Categories".id = "QuoteCategories"."CategoryId"
+GROUP BY
+    "Categories".id, "Categories".name
+ORDER BY
+    qty_of_quotes DESC
+LIMIT
+    100;
+
+
+
+-- Find total qty of the categories which have just one quote
+-- You can change number to other to find info for other categories
+SELECT
+    COUNT(*) AS total_categories_with_one_quote
+FROM
+    (
+        SELECT
+            "CategoryId"
+        FROM
+            "QuoteCategories"
+        GROUP BY
+            "CategoryId"
+        HAVING
+            COUNT(*) = 1
+    ) AS single_quote_categories;
+
+
+-- Find names of the categories which appear only in one quote
+-- You could modify 1 to other value to see categories which appear in multiple quotes (for example 5)
+SELECT
+    "Categories".name AS category_name
+FROM
+    (
+        SELECT
+            "CategoryId"
+        FROM
+            "QuoteCategories"
+        GROUP BY
+            "CategoryId"
+        HAVING
+            COUNT(*) = 1
+    ) AS single_quote_categories
+JOIN
+    "Categories" ON "Categories".id = single_quote_categories."CategoryId";
