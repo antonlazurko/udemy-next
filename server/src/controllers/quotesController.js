eda5fb283c44cd773f59004fe61f015c45db41d1
Bogdan Stashchuk <bstashchuk@gmail.com>
2024-05-19 10:38:28 +0100

    Create separate service for the quotes and move there logic related to the interaction with DB

--- server/src/controllers/quotesController.js
+++ server/src/controllers/quotesController.js
@@ -1,49 +1,10 @@
-const { Op } = require('sequelize');
-const Quote = require('../models/Quote');
-const Category = require('../models/Category');
-
-const attributes = { exclude: ['createdAt', 'updatedAt'] };
-
-const includeCategoryConfig = {
-  model: Category,
-  attributes: ['name'],
-  through: { attributes: [] },
-};
+const { findQuotes, findSingleQuote } = require('../services/quotesService');
 
 const getAllQuotes = async (req, res) => {
   const { limit = 5, offset = 0, author, text, category } = req.query;
-
-  const whereClause = {};
-  author && (whereClause.author = { [Op.iLike]: `%${author}%` });
-  text && (whereClause.text = { [Op.iLike]: `%${text}%` });
-
   try {
-    const quotes = await Quote.findAll({
-      attributes,
-      limit,
-      offset,
-      order: [['id', 'ASC']],
-      include: {
-        ...includeCategoryConfig,
-        where: category ? { name: category } : {},
-      },
-      where: whereClause,
-    });
-
-    // TODO: Try to find the way to filter by category name and find
-    // names of all categories for the quote in one DB request
-    if (!category) {
-      res.json(quotes);
-    } else {
-      const quotesIds = quotes.map((quote) => quote.id);
-      const quotesByIds = await Quote.findAll({
-        attributes,
-        order: [['id', 'ASC']],
-        include: includeCategoryConfig,
-        where: { id: quotesIds },
-      });
-      res.json(quotesByIds);
-    }
+    const quotes = await findQuotes({ limit, offset, author, text, category });
+    res.json(quotes);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -52,11 +13,7 @@ const getAllQuotes = async (req, res) => {
 const getQuoteById = async (req, res) => {
   const quoteId = req.params.id;
   try {
-    const quote = await Quote.findByPk(quoteId, {
-      attributes,
-      include: includeCategoryConfig,
-    });
-
+    const quote = await findSingleQuote(quoteId);
     if (quote) {
       res.json(quote);
     } else {
---
+++ server/src/services/quotesService.js
@@ -0,0 +1,53 @@
+const { Op } = require('sequelize');
+const Quote = require('../models/Quote');
+const Category = require('../models/Category');
+
+const attributes = { exclude: ['createdAt', 'updatedAt'] };
+const includeCategoryConfig = {
+  model: Category,
+  attributes: ['name'],
+  through: { attributes: [] },
+};
+
+const findQuotes = async ({ limit, offset, author, text, category }) => {
+  const whereClause = {};
+  if (author) whereClause.author = { [Op.iLike]: `%${author}%` };
+  if (text) whereClause.text = { [Op.iLike]: `%${text}%` };
+
+  const quotes = await Quote.findAll({
+    attributes,
+    limit,
+    offset,
+    order: [['id', 'ASC']],
+    include: {
+      ...includeCategoryConfig,
+      where: category ? { name: category } : {},
+    },
+    where: whereClause,
+  });
+
+  // TODO: Try to find the way to filter by category name and find
+  // names of all categories for the quote in one DB request
+  if (!category) {
+    return quotes;
+  } else {
+    const quotesIds = quotes.map((quote) => quote.id);
+    const quotesByIds = await Quote.findAll({
+      attributes,
+      order: [['id', 'ASC']],
+      include: includeCategoryConfig,
+      where: { id: quotesIds },
+    });
+    return quotesByIds;
+  }
+};
+
+const findSingleQuote = async (id) => {
+  const quote = await Quote.findByPk(id, {
+    attributes,
+    include: includeCategoryConfig,
+  });
+  return quote;
+};
+
+module.exports = { findQuotes, findSingleQuote };
