ed48cc52cd7cf8bdd06b6de0e96d6211aa76d079
Bogdan Stashchuk <bstashchuk@gmail.com>
2024-09-17 10:44:38 +0100

    Enable cors for all external requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const jsonMiddleware = require('./middlewares/jsonMiddleware');
+const corsMiddleware = require('./middlewares/corsMiddleware');
 const quotesRouter = require('./routes/quotesRouter');
 const categoriesRouter = require('./routes/categoriesRouter');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
+app.use(corsMiddleware);
 app.use(jsonMiddleware);
 
 app.use('/quotes', quotesRouter);
